876b57b8bf876223841cc1069175d2ed21bb5be9
Martin Resnicoff <mresnicoff@gmail.com>
2023-02-17 17:50:10 -0300

    update 4 build for gh-pages

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -20,7 +20,7 @@ const Home = () => {
           
           </p>
           <div>
-          <Link to="/portfolio" 
+          <Link to="/myportfolio" 
 
               className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer"
             >
